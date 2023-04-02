2601cd647565e967bc493298d73425c744c55629
Shmarte1970 <shmarte@gmail.com>
2023-04-02 12:58:57 +0200

    subido desde Git PedroRios1970

---
+++ Irregulars/data/data.js
@@ -0,0 +1,815 @@
+
+
+export const data = [
+
+    {
+        question:'E A T  (Comer)',
+        choise:[
+            'ate',
+            'feed',
+            'bet'
+        ],
+        answer: 'ate'
+
+    },
+
+    {
+        question:'L O S E (Perder)',
+        choise:[
+            'lost',
+            'lose',
+            'let'
+        ],
+        answer: 'lost'
+
+    },
+
+    {
+        question:'H A V E (Tener)',
+        choise:[
+            'have',
+            'haven',
+            'had'
+        ],
+        answer: 'had'
+
+    },
+    {
+        question:'S P E A K (Hablar)',
+        choise:[
+            'speaking',
+            'spoke',
+            'speak'
+        ],
+        answer: 'spoke'
+
+    },
+
+    {
+        question:'S L E E P (Dormir)',
+        choise:[
+            'slept',
+            'loss',
+            'sleep'
+        ],
+        answer: 'slept'
+
+    },
+
+    {
+        question:'D R I N K (Beber)',
+        choise:[
+            'drinking',
+            'drinked',
+            'drank'
+        ],
+        answer: 'drank'
+
+    },
+
+    {
+        question:'S A Y (Decir)',
+        choise:[
+            'see',
+            'saying',
+            'said'
+        ],
+        answer: 'said'
+
+    },
+
+    {
+        question:'B R E A K (Romper)',
+        choise:[
+            'broken',
+            'broke',
+            'broked'
+        ],
+        answer: 'broke'
+
+    },
+
+    {
+        question:'W R I T E (Escribe)',
+        choise:[
+            'wrote',
+            'write',
+            'writing'
+        ],
+        answer: 'wrote'
+
+    },
+
+    {
+        question:'G O (Vamos)',
+        choise:[
+            'going',
+            'went',
+            'go to'
+        ],
+        answer: 'went'
+
+    },
+
+    {
+        question:'G E T (Obtener)',
+        choise:[
+            'got',
+            'goted',
+            'goting'
+        ],
+        answer: 'got'
+
+    },
+
+    {
+        question:'F L Y (Volar)',
+        choise:[
+            'flew',
+            'fling',
+            'fly'
+        ],
+        answer: 'flew'
+
+    },
+
+    {
+        question:'D O (Hacer)',
+        choise:[
+            'did',
+            'does',
+            'doed'
+        ],
+        answer: 'did'
+
+    },
+
+    {
+        question:'P A Y (Pagar)',
+        choise:[
+            'pyment',
+            'pading',
+            'paid'
+        ],
+        answer: 'paid'
+
+    },
+
+    {
+        question:'R E A D (Leer)',
+        choise:[
+            'read',
+            'reading',
+            'reader'
+        ],
+        answer: 'read'
+
+    },
+    {
+        question:'M E E T (Reunirse)',
+        choise:[
+            'meet',
+            'met',
+            'meant'
+        ],
+        answer: 'met'
+
+    },
+    {
+        question:'W I N (Victoria)',
+        choise:[
+            'won',
+            'win',
+            'worn'
+        ],
+        answer: 'won'
+
+    },
+     {
+        question:'P L A Y E D (Jugar)',
+        choise:[
+            'play',
+            'played',
+            'playing'
+        ],
+        answer: 'played'
+
+    },
+    {
+        question:'B U Y (Comprar)',
+        choise:[
+            'bought',
+            'buy',
+            'bougt'
+        ],
+        answer: 'bought'
+
+    },
+    {
+        question:'C A T C H (Capturar)',
+        choise:[
+            'catching',
+            'caught',
+            'cat'
+        ],
+        answer: 'caught'
+
+    },
+    {
+        question:'J O I N S (Unir)',
+        choise:[
+            'join',
+            'joined',
+            'joining'
+        ],
+        answer: 'joined'
+
+    },
+    {
+        question:'L I V E (Vivir)',
+        choise:[
+            'living',
+            'lived',
+            'lives'
+        ],
+        answer: 'lived'
+
+    }, 
+    {
+        question:'A R R I V E (Llegar)',
+        choise:[
+            'arrived',
+            'to arrive'
+        ],
+        answer: 'arrived'
+
+    },
+    {
+        question:'rain (Lluvia)',
+        choise:[
+            'rained',
+            'to rain'
+            
+        ],
+        answer: 'rained'
+
+    },
+    {
+        question:'walks (Camina)',
+        choise:[
+            'walked',
+            'walking'
+            
+        ],
+        answer: 'walked'
+
+    },
+    {
+        question:'G I V E S (Dar)',
+        choise:[
+            'gave',
+            'giving',
+            'given'
+        ],
+        answer: 'gave'
+
+    },
+    {
+        question:'B E A T (Derrotar)',
+        choise:[
+            'beaten',
+            'beating',
+            'beat'
+        ],
+        answer: 'beat'
+
+    },
+    {
+        question:'B E C O M E (Convertirse en)',
+        choise:[
+            'became',
+            'becoming',
+            'becomes'
+        ],
+        answer: 'became'
+
+    },
+    {
+        question:'H I T (Pegar)',
+        choise:[
+            'hits',
+            'hit',
+            'hitting'
+        ],
+        answer: 'hit'
+
+    },
+    {
+        question:'F A L L (Caer)',
+        choise:[
+            'fell',
+            'falling',
+            'falls'
+        ],
+        answer: 'fell'
+
+    },
+    {
+        question:'T E L L (Decir)',
+        choise:[
+            'told',
+            'tells',
+            'telling'
+        ],
+        answer: 'told'
+
+    },
+    {
+        question:'M A K E (Hacer)',
+        choise:[
+            'made',
+            'makeing',
+            'makes'
+        ],
+        answer: 'made'
+
+    },
+    {
+        question:'T A K E (Tomar)',
+        choise:[
+            'took',
+            'takes',
+            'taking'
+        ],
+        answer: 'took'
+
+    },
+    {
+        question:'R U N (Correr)',
+        choise:[
+            'run',
+            'running',
+            'ran'
+        ],
+        answer: 'ran'
+
+    },
+    {
+        question:'H O L D (Mantener)',
+        choise:[
+            'held',
+            'holds',
+            'heldes'
+        ],
+        answer: 'held'
+
+    },
+    {
+        question:'Know (Saber)',
+        choise:[
+            'knows',
+            'knew',
+            'knowing'
+        ],
+        answer: 'knew'
+
+    },
+    {
+        question:'L E A V E (Abandonar)',
+        choise:[
+            'left',
+            'leave',
+            'leaving'
+        ],
+        answer: 'left'
+
+    },
+    {
+        question:'D R I V E (Conducir)',
+        choise:[
+            'drove',
+            'droves',
+            'drive'
+        ],
+        answer: 'drove'
+
+    },
+    {
+        question:'D R A W (Dibujar)',
+        choise:[
+            'drew',
+            'drawing',
+            'draws'
+        ],
+        answer: 'drew'
+
+    },
+    {
+        question:'B E T (Apostar)',
+        choise:[
+            'bets',
+            'beating',
+            'bet'
+        ],
+        answer: 'bet '
+
+    },
+    {
+        question:'F E E L (Sentir)',
+        choise:[
+            'felt',
+            'feling',
+            'feel'
+        ],
+        answer: 'felt'
+
+    },
+    {
+        question:'P U T (Poner)',
+        choise:[
+            'put',
+            'putting',
+            'puts'
+        ],
+        answer: 'put'
+
+    },
+    {
+        question:'B E G I N (Empezar)',
+        choise:[
+            'began',
+            'beganing',
+            'begans'
+        ],
+        answer: 'began'
+
+    },
+    {
+        question:'G R O W (Crecer)',
+        choise:[
+            'grew',
+            'grewing',
+            'grews'
+        ],
+        answer: 'grew'
+
+    },
+    {
+        question:'C O S T (Costar)',
+        choise:[
+            'costing',
+            'cost',
+            'cust'
+        ],
+        answer: 'cost'
+
+    },
+    {
+        question:'H I D E (Ocultar)',
+        choise:[
+            'hid',
+            'hide',
+            'hiden'
+        ],
+        answer: 'hid'
+
+    },
+    {
+        question:'U N D E R S T A N D (Entender)',
+        choise:[
+            'understood',
+            'understod',
+            'understop'
+        ],
+        answer: 'understood'
+
+    },
+    {
+        question:'C H O O S E (Elegir)',
+        choise:[
+            'chose',
+            'chuse',
+            'choose'
+        ],
+        answer: 'chose'
+
+    },
+    {
+        question:'K E E P (Mantener)',
+        choise:[
+            'kept',
+            'keeps',
+            'keeping'
+        ],
+        answer: 'kept'
+
+    },
+    {
+        question:'F O R G I V E (Perdonar)',
+        choise:[
+            'forgave',
+            'forgive',
+            'forgiving'
+        ],
+        answer: 'forgave'
+
+    },
+    {
+        question:'B E N D (Doblar)',
+        choise:[
+            'bent',
+            'bending',
+            'beens'
+        ],
+        answer: 'bent'
+
+    },
+    {
+        question:'W E A R (Llevar Puesto)',
+        choise:[
+            'wear',
+            'wire',
+            'wore'
+        ],
+        answer: 'wore'
+
+    },
+    {
+        question:'H E A R (Oir)',
+        choise:[
+            'heard',
+            'hear'
+        ],
+        answer: 'heard'
+
+    },
+    {
+        question:'H U R T (Herida)',
+        choise:[
+            'hurt',
+            'hurting',
+            'hart'
+        ],
+        answer: 'hurt'
+
+    },
+    {
+        question:'M E A N (Media)',
+        choise:[
+            'mean',
+            'meant',
+            'meanting'
+        ],
+        answer: 'meant'
+
+    },
+    {
+        question:'L A Y (Poner)',
+        choise:[
+            'laid',
+            'lay',
+            'lais'
+        ],
+        answer: 'laid'
+
+    },
+    {
+        question:'C U T (Corte)',
+        choise:[
+            'cut',
+            'cuting',
+            'cat'
+        ],
+        answer: 'cut'
+
+    },
+    {
+        question:'F I G H T (Luchar)',
+        choise:[
+            'fighting',
+            'fought',
+            'fight'
+        ],
+        answer: 'fought'
+
+    },
+    {
+        question:'B I T E (Morder)',
+        choise:[
+            'bite',
+            'bit',
+            'biting'
+        ],
+        answer: 'bit'
+
+    },
+    {
+        question:'F I N D (Encontrar)',
+        choise:[
+            'found',
+            'find'
+            
+        ],
+        answer: 'found'
+
+    },
+    {
+        question:'L E A D (Guiar)',
+        choise:[
+            'leading',
+            'lead',
+            'led'
+        ],
+        answer: 'led'
+
+    },
+    {
+        question:'L E N D (Prestar)',
+        choise:[
+            'lent',
+            'lenting',
+            'lens'
+        ],
+        answer: 'lent'
+
+    },
+    {
+        question:'B L E E D (Sangrar)',
+        choise:[
+            'bled',
+            'bleding',
+            'bleds'
+        ],
+        answer: 'bled'
+
+    },
+    {
+        question:'S E L L (Vender)',
+        choise:[
+            'sold',
+            'sell',
+            'selling'
+        ],
+        answer: 'sold'
+
+    },
+    {
+        question:'R I S E (Elevar)',
+        choise:[
+            'rose',
+            'rise',
+            'riising'
+        ],
+        answer: 'rose'
+
+    },
+    {
+        question:'F E E D (Alimentar)',
+        choise:[
+            'fed',
+            'feed',
+            'feding'
+        ],
+        answer: 'fed'
+
+    },
+    {
+        question:'B L O W (Explotar)',
+        choise:[
+            'blew',
+            'blow',
+            'blewed'
+        ],
+        answer: 'blew'
+
+    },
+    {
+        question:'C O M E (Venir)',
+        choise:[
+            'came',
+            'come',
+            'comed'
+        ],
+        answer: 'came'
+
+    },
+    {
+        question:'D I G (Dirigir)',
+        choise:[
+            'dug',
+            'dig',
+            'dign'
+        ],
+        answer: 'dug'
+
+    },
+    {
+        question:'B R E E D (Criar)',
+        choise:[
+            'bred',
+            'breds',
+            'breding'
+        ],
+        answer: 'bred'
+
+    },
+    {
+        question:'F O R G E R (Clasificar)',
+        choise:[
+            'forgot',
+            'for',
+            'forgoting'
+        ],
+        answer: 'forgot'
+
+    },
+    {
+        question:'B R I N D (Traer)',
+        choise:[
+            'brought',
+            'bring',
+            'briting'
+        ],
+        answer: 'brought'
+
+    },
+    { 
+        question:'S I T (Sentar)',
+        choise:[
+            'sat',
+            'sit',
+            'siting'
+        ],
+        answer: 'sat'
+
+    },
+    {
+        question:'F R E E Z E (Congelar)',
+        choise:[
+            'froze',
+            'freeze',
+            'frozes'
+        ],
+        answer: 'froze'
+
+    },
+    {
+        question:'T H I N K (Pensar)',
+        choise:[
+            'thinking',
+            'thought',
+            'think'
+        ],
+        answer: 'thought'
+
+    },
+    {
+        question:'B U I L d (Construir)',
+        choise:[
+            'built',
+            'building',
+            'build'
+        ],
+        answer: 'built'
+
+    },
+    {
+        question:'T E A C H (Enseñar) ',
+        choise:[
+            'taught',
+            'teaching',
+            'teach'
+        ],
+        answer: 'taught'
+
+    },
+    {
+        question:'S P E N D (Gastar)',
+        choise:[
+            'spent',
+            'spend',
+            'speding'
+        ],
+        answer: 'spent'
+
+    },
+    {
+        question:'D R E A M (Soñar)',
+        choise:[
+            'dreamt',
+            'dreams',
+            
+        ],
+        answer: 'dreamt'
+
+    }
+    
+
+
+
+
+
+
+
+]
+
---
+++ verbs/data/data.js
@@ -0,0 +1,342 @@
+
+
+export const data = [
+
+    {
+        question:'Walk (Caminar) ',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+    {
+        question:'DO (Hacer)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'H A V E (Tener)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'W A T C H  (Mirar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+    {
+        question:'C L E A N (Limpio)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+    {
+        question:'D R I N K (Beber)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'S A Y (Decir)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'P L A Y (jugar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+    {
+        question:'W R I T E (Escribe)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'G O (Vamos)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'G O T (Obtener)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'J U M P  (Saltar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+    {
+        question:'D O (Hacer)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'P A Y (Pagar)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'R E A D (Leer)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'L O O K (Mirar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+    {
+        question:'M E T (Reunirse)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'N E D (Necesidad)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+     {
+        question:'L I V E (vivir)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+    {
+        question:'B U Y (Comprar)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+    },
+    {
+        question:'D R I V E R (Conducir)',
+        choise:[
+            'regular',
+            'irregular'
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'S E A R C H (Buscar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+    {
+        question:'M A K E (Hacer)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    }, 
+    {
+        question:'C A R R I E (Llegar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+    {
+        question:'F I N D (Encontrar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'L E A V E (Dejar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'S T A R T (Empezar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+
+
+    {
+        question:'G I V E S (Dar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'S E E (Ver)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+    {
+        question:'R E C I E V E (Recivir)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+
+    },
+    {
+        question:'W E R E (Eran)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'T A K E  (llevar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'irregular'
+
+    },
+
+    {
+        question:'F I N S H (Finalizar)',
+        choise:[
+            'regular',
+            'irregular'
+            
+        ],
+        answer: 'regular'
+    },
+  
+
+]
+
